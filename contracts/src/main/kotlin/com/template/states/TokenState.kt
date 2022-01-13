b40ceb4ad33e37cfe54f09894b1b46bc99623af5
ymiyata-cac <ymiyata@cac.co.jp>
2022-01-13 10:47:27 +0900

    add state test

---
+++ contracts/src/main/kotlin/com/template/states/TokenState.kt
@@ -0,0 +1,14 @@
+package com.template.states
+
+import net.corda.core.contracts.BelongsToContract
+import net.corda.core.contracts.ContractState
+import net.corda.core.identity.AbstractParty
+import net.corda.core.identity.Party
+
+//@BelongsToContract(TokenContract::class)
+data class TokenState(
+    val sender: Party,
+    val receiver: Party,
+    val amount: Int,
+    override val participants: List<AbstractParty> = listOf(sender, receiver)
+) : ContractState
--- contracts/src/test/kotlin/com/template/contracts/StateTests.kt
+++ contracts/src/test/kotlin/com/template/contracts/StateTests.kt
@@ -1,15 +1,27 @@
 package com.template.contracts
 
-import com.template.states.TemplateState
+import com.template.states.TokenState
+import net.corda.core.identity.CordaX500Name
+import net.corda.core.identity.Party
+import net.corda.testing.core.TestIdentity
 import org.junit.Test
-import kotlin.test.assertEquals
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 
 class StateTests {
-    @Test
-    fun hasFieldOfCorrectType() {
-        // Does the field exist?
-        TemplateState::class.java.getDeclaredField("msg")
-        // Is the field of the correct type?
-        assertEquals(TemplateState::class.java.getDeclaredField("msg").type, String()::class.java)
+    private val alice: Party = TestIdentity(CordaX500Name("Alice", "Tokyo", "JP")).party
+    private val bob: Party = TestIdentity(CordaX500Name("Bob", "NewYork", "US")).party
+
+//    @Test // 正しい型のパラメータを持つこと
+//    fun tokenStateHasParamsOfCorrectTypeInConstructor() {
+//        TokenState(alice, bob, 100)
+//    }
+
+    @Test // senderとreceiverがParticipantsであること
+    fun tokenStateHasTwoParticipantsTheSenderAndTheReceiver() {
+//        val tokenState = TokenState(alice, bob, 100)
+//        assertEquals(2, tokenState.participants.size)
+//        assertTrue(tokenState.participants.contains(alice))
+//        assertTrue(tokenState.participants.contains(bob))
     }
 }
